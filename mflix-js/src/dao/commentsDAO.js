7f87fed6702671a45ccecff28a8d7e18d17a94ec
Soros Liu <soros.liu1029@gmail.com>
2021-09-28 12:29:13 +0800

    finish ticket: read concerns

--- mflix-js/src/dao/commentsDAO.js
+++ mflix-js/src/dao/commentsDAO.js
@@ -123,11 +123,20 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: User Report
       // Return the 20 users who have commented the most on MFlix.
-      const pipeline = []
+      const pipeline = [
+        {
+          $group: {
+            _id: "$email",
+            count: { $sum: 1 },
+          },
+        },
+        { $sort: { count: -1 } },
+        { $limit: 20 },
+      ]
 
       // TODO Ticket: User Report
       // Use a more durable Read Concern here to make sure this data is not stale.
-      const readConcern = comments.readConcern
+      const readConcern = "majority"
 
       const aggregateResult = await comments.aggregate(pipeline, {
         readConcern,
