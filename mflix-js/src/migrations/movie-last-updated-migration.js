63218467e4424c08707f0d50835f21766da63021
Soros Liu <soros.liu1029@gmail.com>
2021-09-28 14:20:00 +0800

    finish ticket: migration

--- mflix-js/src/migrations/movie-last-updated-migration.js
+++ mflix-js/src/migrations/movie-last-updated-migration.js
@@ -28,8 +28,8 @@ require("dotenv").config()
     // check that its type is a string
     // a projection is not required, but may help reduce the amount of data sent
     // over the wire!
-    const predicate = { somefield: { $someOperator: true } }
-    const projection = {}
+    const predicate = { lastupdated: { $exists: true, $type: "string" } }
+    const projection = { lastupdated: 1, _id: 1 }
     const cursor = await mflix
       .collection("movies")
       .find(predicate, projection)
@@ -47,7 +47,9 @@ require("dotenv").config()
       `Found ${moviesToMigrate.length} documents to update`,
     )
     // TODO: Complete the BulkWrite statement below
-    const { modifiedCount } = await "some bulk operation"
+    const { modifiedCount } = await mflix
+      .collection("movies")
+      .bulkWrite(moviesToMigrate)
 
     console.log("\x1b[32m", `${modifiedCount} documents updated`)
     client.close()
